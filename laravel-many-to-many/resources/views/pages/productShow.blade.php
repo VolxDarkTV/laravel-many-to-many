0e2f8bfdea7da40ac91b6fd45133c149ab115f41
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-02-15 13:22:21 +0100

    add element in productShow

--- laravel-many-to-many/resources/views/pages/productShow.blade.php
+++ laravel-many-to-many/resources/views/pages/productShow.blade.php
@@ -8,7 +8,13 @@
     <section class="container-fluid">
         <div class="container">
             <h2>Show</h2>
-            <span>{{$product -> name}}</span>
+            <div>
+                <h6>{{$product -> name}}</h6>
+                <h6>{{$product -> description}}</h6>
+                <h6>{{$product -> price}}</h6>
+                <h6>{{$product -> weight}}</h6>
+            </div>
+
         </div>
     </section>
 @endsection
